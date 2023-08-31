069deee7e66f26c17b7a4eb76ec9b702223c191b
JackM0 <jackfmoody@gmail.com>
2023-08-31 16:51:03 +1000

    FEAT: Conversion to greyscale

--- ICA.py
+++ ICA.py
@@ -60,9 +60,6 @@ class ICA:
             self.labels = _onehot(self.labels)
 
 
-    def PerformICA(self):
-        self.ICA = self.transformer.fit_transform(self.images[0:20, :, :, :])
-
     def TripleChannelUnflatten(self):
         self.channel_1 = self.dataset[:, 0 : 1024].reshape(60000, 32, 32)
         self.channel_2 = self.dataset[:, 1024 : 2 * 1024].reshape(60000, 32, 32)
@@ -71,8 +68,18 @@ class ICA:
         self.images[:, :, :, 0] = self.channel_1
         self.images[:, :, :, 1] = self.channel_2
         self.images[:, :, :, 2] = self.channel_3
+
+        self.rgb2gray()
+
         return
     
+    def rgb2gray(self):
+        r, g, b = self.channel_1, self.channel_2, self.channel_3
+        self.gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+
+    def PerformICA(self):
+        self.ICA = self.transformer.fit_transform(ica.gray[0:20, :, :])
 
 if __name__ == '__main__':
     tar = 'cifar-10-binary.tar.gz'
@@ -88,9 +95,12 @@ if __name__ == '__main__':
     print(ica.dataset.shape)
     print(ica.labels)
     ica.TripleChannelUnflatten()
-    print(ica.images.shape)
-    # ica.dataset = ica.dataset[0:20, :]
     ica.PerformICA()
+    print(ica.gray.shape)
+    plt.imshow(np.abs(np.fft.fftshift(np.fft.fft2(ica.gray[0]))))
+    plt.show()
+    # ica.dataset = ica.dataset[0:20, :]
+   
     # print(ica.ICA)
     
 
