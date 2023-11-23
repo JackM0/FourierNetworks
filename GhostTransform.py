e553588d445694941dd33978786f1c7a4f0220b7
JackM0 <jackfmoody@gmail.com>
2023-11-24 00:02:14 +1000

    Adjustments to sort based on power, added reconstruction error

--- GhostTransform.py
+++ GhostTransform.py
@@ -135,7 +135,7 @@ class GhostTransform:
         # Define the dimensions of the mask (e.g., 8x8)
         width, height = 1000, 1000
         rotation_angle_degrees = 45  # Change the angle as desired
-        sizes = [5, 10, 15]
+        sizes = [10]
         hermite_constructor.CreateHermiteOfMaxOrder(width, height, hermite_order, rotation_angle_degrees, sizes)
         
         if(not os.path.exists(results_directory)):
@@ -184,7 +184,7 @@ class GhostTransform:
         # plt.plot(np.flip(np.sort(compression)))
         # plt.show()
 
-        
+        recon_error = np.zeros((images_to_decompose.shape[0]))
         args = self.EnergyHermiteCoefficients(coefficients)
         
         reconstructed_images = np.zeros((images_to_decompose.shape[0], 32, 32))
@@ -192,30 +192,49 @@ class GhostTransform:
             #print(i)
             for k, j in enumerate(args): #enumerate(hermite_constructor.hermite_functions):
                 image += hermite_constructor.hermite_functions[j, 484 : 516, 484 : 516] * coefficients[i, j]
-                
+            error = np.abs((image - np.real(images_to_decompose[i]))) / (32 * 32)
+            recon_error[i] = np.sum(np.sum(error))
+        
+
+        print(recon_error)
+
         self.SaveAllImages(reconstructed_images, 'reconstructed', results_directory)
         self.SaveAllImages(np.real(images_to_decompose), 'original', results_directory)
         self.SaveAllImages(hermite_constructor.hermite_functions[:, 484 : 516, 484 : 516], 'hermite_functions', results_directory)
 
+        plt.plot(recon_error, marker='o')
+        plt.xlabel('Basis Vectors')
+        plt.ylabel('Cumulative Power')
+        plt.title('Power Energy Analysis')
+        plt.show()
         
 
     def EnergyHermiteCoefficients(self, coefficient_matrix):
         variance_per_basis = np.var(coefficient_matrix, axis=0)
-        args = np.flip(np.argsort(variance_per_basis))
+        power_per_basis = np.sum(np.square(np.abs(coefficient_matrix)), axis = 0)
+        args_var = np.flip(np.argsort(variance_per_basis))
+        args_power = np.flip(np.argsort(power_per_basis))
         
-        cumulative_variance = np.cumsum(variance_per_basis[args]) / np.sum(variance_per_basis)
+        cumulative_variance = np.cumsum(variance_per_basis[args_var]) / np.sum(variance_per_basis)
+        cumulative_power = np.cumsum(power_per_basis[args_power]) / np.sum(power_per_basis)
         plt.plot(cumulative_variance, marker='o')
         plt.xlabel('Basis Vectors')
         plt.ylabel('Cumulative Variance')
         plt.title('Variance Energy Analysis')
         plt.show()
 
+        plt.plot(cumulative_power, marker='o')
+        plt.xlabel('Basis Vectors')
+        plt.ylabel('Cumulative Power')
+        plt.title('Power Energy Analysis')
+        plt.show()
 
-        for i in range(len(args)):
-            if cumulative_variance[i] > 0.8:
+
+        for i in range(len(args_power)):
+            if cumulative_power[i] > 0.9:
                 break
         print(i)
-        return args[:i]
+        return args_power[:i]
             
 if __name__ == '__main__':
     tar = 'cifar-10-binary.tar.gz'
@@ -273,9 +292,9 @@ if __name__ == '__main__':
     # ghost_transform.SaveAllImages(np.real(ghost_transform.RF_basis_images_fft), "rf_fft_real_", location)
     # ghost_transform.SaveAllImages(np.imag(ghost_transform.RF_basis_images_fft), "rf_fft_imag_", location)
 
-    hermite_order = 20
+    hermite_order = 40
     # ghost_transform.DecomposeGhosts(10, ghost_transform.constructor.ghost_images)
-    ghost_transform.DecomposeImages(hermite_order, ghost_transform.constructor.receptive_field_images, './rf_recon_' + 'order_' + str(hermite_order) + '_3sizes')
+    ghost_transform.DecomposeImages(hermite_order, ghost_transform.constructor.receptive_field_images, './rf_recon_' + 'order_' + str(hermite_order))
 
     #ghost_transform.ConstructGramMatrix(ghost_transform.basis_images)
     #ghost_transform.LinearDecompositionGhosts()
